1958370e8810a7b2ee1a62b431a8e3ce47c860dc
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2026-01-30 15:09:43 +0100

    feat: response scoring endpoint for user feedback.

---
+++ Backend/src/routes/feedback.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { firebaseService } from '../services/firebaseService.js';
+
+const router = express.Router();
+
+router.post('/api/feedback', async (req, res) => {
+    const { userId, responseId, humaneScore, accuracyScore, improvement } = req.body;
+
+    try {
+        await firebaseService.createDocument('feedback', {
+            userId,
+            humaneScore,
+            accuracyScore,
+            improvement,
+            timestamp: new Date()
+        }, responseId);
+
+        console.log("Schedule created successfully");
+
+        res.status(200).json({ message: 'Feedback recorded successfully' });
+
+    } catch (error) {
+        console.error('Error creating schedule:', error);
+        res.status(500).json({ error: 'Failed to create schedule' });
+    }
+})
